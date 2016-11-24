5f74a186ee61791aa237dbe4a1325d584b2c5411
Lowkyu <rokyu25@gmail.com>
2016-11-24 17:07:16 +0900

    webdb
    fasd

---
+++ php/test200.php
@@ -0,0 +1,177 @@
+﻿<html>
+<head>
+<title>test200.php</title>
+<link rel="stylesheet" type="text/css" href="../css/test_style.css">
+</head>
+<body>
+<h1>第１章 Web-DB連携の動作確認</h1>
+<h2>ブラウザからデータベース（DB）への接続確認</h2>
+<hr />
+<div style="width:700px;margin:auto">
+<form method="post" action="test201.php" style="margin:auto">
+<table id="tblQ">
+<tr>
+	<th colspan="2"><h3>基本情報</h3></th>
+</tr>
+<!--出席番号-->
+<tr>
+<td>
+<span class="question">出席番号</span>
+</td>
+<td>
+<select name="frmAtdnum" size="1">
+<script>
+for(var i = 1;i<=39;i++){
+	document.write('<option value="'+i+'">'+i+'</option>\n');
+}
+</script>
+</select>
+</td>
+</tr>
+ <!--名前-->
+<tr>
+<td>
+<span class="question">名前</span>
+</td>
+<td>
+<input type="text" placeholder="漢字で名前を入力してください" name="frmName" >
+</td>
+</tr>
+ <!--性別-->
+<tr>
+<td>
+<span class="question">性別</span>
+</td>
+<td>
+<input type="radio" id="male" name="frmGender" value="男">
+<label for="male">男性</label>
+<input type="radio" id="female" name="frmGender" value="女">
+<label for="female">女性</label><br>
+<input type="radio" name="frmGender"value="" checked="checked" style="display:none;" />
+</td>
+</tr>
+<!--誕生月-->
+<tr>
+<td>
+<span class="question">誕生日</span>
+</td>
+<td>
+<select name="frmBirthMonth">
+<option value=""> </option>;
+<script>
+for(var i = 1;i<=12;i++){
+	document.write("<option value='"+i+"'>"+i+"</option>");
+}
+</script>
+</select>月
+<!--誕生日-->
+<select name="frmBirthDate">
+<option value=""> </option>;
+<script>
+for(var i = 1;i<=31;i++){
+	document.write("<option value='"+i+"'>"+i+"</option>");
+}
+</script>
+</select>日</td>
+<!--血液型-->
+<tr>
+<td>
+<span class="question">血液型</span>
+</td>
+<td>
+<select name="frmBlood">
+<option value=""> </option>;
+<option value="A">A</option>
+<option value="B">B</option>
+<option value="O">O</option>
+<option value="AB">AB</option>
+</select>型
+</td>
+</tr>
+<!--身長-->
+<tr>
+<td>
+<span class="question">身長</span>
+</td>
+<td>
+<select name="frmTall">
+<option value=""></option>;
+<script>
+var tallMax = 130;
+var i= 0;
+var val=new Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S");
+document.write('<option value="'+val[i]+'">～'+tallMax+'</option>');
+while(tallMax <=210){
+	tallMax= tallMax+5;
+	i= i+1;
+	var tallMin=tallMax-4;
+	document.write('<option value="'+val[i]+'">'+tallMin+'～'+tallMax+'</option>');
+}
+document.write('<option value="'+val[i+1]+'">'+tallMax+'～</option>');
+</script>
+</select>
+cm
+</td>
+</tr>
+<tr>
+<th colspan="2"><h3>質問</h3></th>
+</tr>
+<!--犬派 or 猫派-->
+<tr>
+<td>
+<span class="question">犬派？猫派？</span>
+</td>
+<td>
+<input type="radio" id="dog" name="frmPet" value="犬">
+<label for="dog">犬</label>
+<input type="radio" id="cat" name="frmPet" value="猫">
+<label for="cat">猫</label>
+</td>
+</tr>
+<!--利き手-->
+<tr>
+<td>
+<span class="question">利き手</span>
+</td>
+<td>
+<input type="radio" id="rightHand" name="frmHand" value="左利き">
+<label for="rightHand">左利き</label>
+<input type="radio" id="leftHand" name="frmHand" value="右利き">
+<label for="leftHand">右利き</label><br>
+<input type="radio" name="frmHand"value="" checked="checked" style="display:none;" />
+</td>
+</tr>
+<!--平均睡眠時間-->
+<tr>
+<td>
+<span class="question">１日の平均睡眠時間</span>
+</td>
+<td>
+<input type="text" plaseholder="半角数字で入力してください" size="2" name="frmSleep">時間
+</td>
+</tr> 
+</table>
+<div style="float:right">
+<input type="submit" value="SEND"><input type="reset" value="CLEAR">
+</div>
+</form>
+
+</div>
+
+
+<?php
+//DB接続
+$conn = pg_connect("dbname=db_3837 user=d33837 host=192.168.109.210");
+
+//SQL発行
+$result = pg_query("select * from tbl_data");
+
+//SQLで取得した行と列
+$cntRows = pg_numRows($result);
+$cntFields = pg_numFields($result);
+
+pg_free_result($result);//テーブル切断
+pg_close($conn);//データベース切断
+?>
+</body>
+</html>
---
+++ php/test201.php
@@ -0,0 +1,125 @@
+﻿<html>
+<head>
+<title>test201.php</title>
+<link rel="stylesheet" type="text/css" href="../css/test_style.css">
+</head>
+<body>
+<h1>第１章 Web-DB連携の動作確認</h1>
+<h2>ブラウザからデータベース（DB）への接続確認</h2>
+<hr />
+<article>
+<?php
+$missing = 0;
+if($_POST["frmName"]!='' and $_POST["frmGender"] !='' and $_POST["frmBirthMonth"]!='' and $_POST["frmBirthDate"]!='' and $_POST["frmBlood"]!='' and $_POST["frmTall"]!='' and $_POST["frmAtdnum"]){
+$atdnum = $_POST["frmAtdnum"];
+if($atdnum < 10){
+	$atdnum = "0".$atdnum;
+}
+$name      = $_POST["frmName"];
+$gender    = $_POST["frmGender"];
+$brthMonth = $_POST["frmBirthMonth"];
+$brthDate  = $_POST["frmBirthDate"];
+$blood     = $_POST["frmBlood"];
+$tall      = $_POST["frmTall"];
+$pet       = $_POST["frmPet"];
+$hand      = $_POST["frmHand"];
+$slpTime   = $_POST["frmSleep"];
+$missing = 1;
+}
+//DB接続
+$conn = pg_connect("dbname=db_3837 user=d33837 host=192.168.109.210");
+//レコード存在確認
+if($missing == 0){
+	print("<h4>入力されていない項目があります</h4>\n");
+	if($_POST["frmName"] =='')$missItem[0] = "名前";
+	if($_POST["frmGender"] =='')$missItem[1] = "性別";
+	if($_POST["frmBirthMonth"] =='')$missItem[2] = "誕生月";
+	if($_POST["frmBirthDate"] =='')$missItem[3] = "誕生日";
+	if($_POST["frmBlood"] =='')$missItem[4] = "血液型";
+	if($_POST["frmTall"] =='')$missItem[5] = "身長";
+	if($_POST["frmPet"] ='')$missItem[6] = "質問１";
+	foreach($missItem as $outItems){
+		print("<font color='red'>".$outItems."</font><br>\n");
+	}
+	print("<a href='test200.php'>戻る</a>");
+//レコード挿入・変更
+}else{
+	$exists = pg_query($conn,"select 出席番号 from tbl_data where 出席番号 ='d338$atdnum'");
+	$exstRow = pg_numRows($exists);
+	if($exstRow == 0){    //レコードがまだ無い場合
+		$record = pg_query("insert into tbl_data values(
+		'd338$atdnum',
+		'$name',
+		'$gender',
+		'$brthMonth',
+		'$brthDate',
+		'$blood',
+		'$tall',
+		'$pet',
+		'$hand',
+		'$slpTime'
+		);");
+		print("<p align='center' style='color:blue'>レコードを追加しました</p>\n");
+	}else{                //レコードが既にある場合
+		$record = pg_query("update tbl_data set 
+		名前 = '$name',
+		性別 = '$gender',
+		誕生月 = $brthMonth,
+		誕生日 = $brthDate,
+		血液型 = '$blood',
+		身長 = '$tall',
+		q1 = '$pet',
+		q2 = '$hand',
+		q3 = '$slpTime'
+		where 出席番号= 'd338$atdnum';
+		");
+		print("<p align='center' style='color:red;'>レコードを更新しました</p>\n");
+	}
+if($missing==1){
+	print("<table id='tblQ' style='width:700px;margin:auto;'>");
+	print("<tr><th colspan='2'><h3>入力情報</h3></th></tr>");
+	print("<tr><td>名前</td><td>".$name."</td></tr>\n");
+	print("<tr><td>出席番号</td><td>".$atdnum."番</td></tr>\n");
+	print("<tr><td>性別</td><td>".$gender."</td></tr>\n");
+	print("<tr><td>誕生月</td><td>".$brthMonth."月</td></tr>\n");
+	print("<tr><td>誕生日</td><td>".$brthDate."日</td></tr>\n");
+	print("<tr><td>血液型</td><td>".$blood."型</td></tr>\n");
+	print("<tr><td>身長</td><td>".$tall."</td></tr>\n");
+	print("<tr><th colspan='2'><h3>質問回答</h3></th></tr>");
+	print("<tr><td>犬派？猫派？</td><td>".$pet."</td></tr>\n");
+	print("<tr><td>利き手</td><td>".$hand."</td></tr>\n");
+	print("<tr><td>１日の平均睡眠時間</td><td>".$slpTime."時間</td></tr>\n");
+	print("</table>");
+}
+//SQL発行
+$result = pg_query("select * from tbl_data order by 出席番号 asc");
+//SQLで取得した行と列
+$cntRows = pg_numRows($result);
+$cntFields = pg_numFields($result);
+
+//テーブル出力
+print("<table id='tblDB'>\n");
+print("<tr>\n");
+for($k = 0;$k<$cntFields;$k++){
+	print("<th>\n");	
+	print(pg_fieldname($result,$k));
+	print("</th>\n");
+}
+for($i = 0;$i < $cntRows;$i++){
+	print("<tr>");
+	for($j =0;$j < $cntFields;$j++){
+		print("<td>");
+		print(pg_result($result,$i,$j));
+		print("</td>");
+	}
+	print("</tr>");
+}
+print("</table>");
+
+pg_free_result($result);//テーブル切断
+pg_close($conn);//データベース切断
+}
+?>
+</article>
+</body>
+</html>
